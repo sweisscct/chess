a16ee7cd4dfe46a3381b3019c49dba72cb96408d
ValentinaPiano313 <sba24012@student.cct.ie>
2024-11-09 11:46:39 +0000

    change turn functions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+// Indicate whose turn it is
+//Turns will be “hot-seat” – players will take turns using the same computer to play
+
+
+function firstTurn(){
+     turn = Math.random();
+     if(turn < 0.5){
+        return 1;
+     }else{
+        return 2;
+     }
+}
+var playerTurn = firstTurn();
+
+function changeTurn(){
+        if (playerTurn == 1){
+            playerTurn = 2;
+        }else{
+            playerTurn = 1;
+        }     
+    return playerTurn
+}
+
+function getTurn() {
+    return playerTurn;
+}
