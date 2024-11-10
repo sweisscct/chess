104557c90922e22d0c663c59621f9ea709a54229
walterfcarvalho@gmail.com <sba24317@student.cct.ie>
2024-11-10 13:48:33 +0000

    functions to update pieces on the board

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-board = [
-    ["t", "h", "b", "k", "q", "b", "h", "t"]
-    ["p", "p", "p", "p", "p", "p", "p", "p"],
-    [ "",  "",  "",  "",  "",  "",  "", ""],
-    [ "",  "",  "",  "",  "",  "",  "", ""],
-    [ "",  "",  "",  "",  "",  "",  "", ""],
-    [ "",  "",  "",  "",  "",  "",  "", ""],
-    ["p", "p", "p", "p", "p", "p", "p", "p"],
-    ["t", "h", "b", "k", "q", "b", "h", "t"]
+let col = 0;
+let row = 0
+
+let images = {
+    "t": "b-hook.png",
+    "h": "b-knight.png",
+    "b": "b-bishop.png",
+    "k": "b-king.png",
+    "q": "b-queen.png",
+    "p": "b-pawn.png",  
+    "T": "w-hook.png",
+    "H": "w-knight.png",
+    "B": "w-bishop.png",
+    "K": "w-king.png",
+    "Q": "w-queen.png",
+    "P": "w-pawn.png"
+}
+
+
+let board = [
+    ["t",   "h",  "b",  "k",  "q",  "b",  "h",  "t"],
+    ["p",   "p",  "p",  "p", null,  "p",  "p",  "p"],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null,  "p", null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ["P",   "P",  "P",  "P",  "P",  "P",  "P",  "P"],
+    ["T",   "H",  "B",  "K",  "Q",  "B",  "H",  "T"]
 ];
 
 
+/*
+    this function will read matrice Board and update html with most updated position
+    game
+*/
+function updateBoard() {
+    for (row = 0; row < board.length; row++)
+        for (col = 0; col < board[0].length; col++) {
+            let src = "transparency-image.png";
+            
+            if (board[row][col] != null) 
+                src = images[board[row][col]];
+
+
+            console.log( "./images/" + src );
+            document.getElementById("" + row + col).setAttribute("src", "./images/" + src );
+        }
+}
+
+function newGame() {
+    
+    // todo
+
+}
+
+
+
+updateBoard();
