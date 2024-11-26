b7ef02b09533e70c798bf81aa615b5b62766d8fb
walterfcarvalho@gmail.com <sba24317@student.cct.ie>
2024-11-26 14:52:29 +0000

    pieces movements and capturing

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// let col = 0;
-// let row = 0
-let isTurnWhite;
-let movesWPawn;
+let is_turn_white;
+let turn_phase; // 1=piece selection | 2=destination selection
 let board = [];
-let square_destination = [];
-let wPawnDirection = 'asc';
-let bPawnDirection = 'desc';
+let square_dests = [];
+let square_orig;
+let w_pawn_direction = 'asc';
+let b_pawn_direction = 'desc';
 
 let images = {
     "t": "b-hook.png",
@@ -22,10 +21,11 @@ let images = {
     "P": "w-pawn.png"
 }
 
+
 let b_pawn = ["p", "p", "p", "p", "p", "p", "p", "p"];
 let b_piece = ["t", "h", "b", "k", "q", "b", "h", "t"];
 let w_pawn = ["P", "P", "P", "P", "P", "P", "P", "P"];
-let w_piece = ["T", "H", "B", "K", "Q", "B", "H", "T"];
+let w_piece = ["T", "H", "B", "Q", "K", "B", "H", "T"];
 let n_line = [null, null, null, null, null, null, null, null];
 
 let moves_knight = [
@@ -53,16 +53,30 @@ moves_bishop.forEach(e => moves_queen.push(e));
 let moves_king = [];
 moves_queen.forEach(e => moves_king.push([e[0]]));
 
+/*
+    handle keyUp on body
+*/
+function bodyKeyUp(target) {
+    console.log(target);
+}
 
+/*
+    resolve square that a pawn can be moved
+*/
 function movesPawn(r, c, pawnDirection) {
     let res = [];
     let auxR;
     let auxC;
+    let direction = pawnDirection == 'desc' ? 1 : -1;
 
     if (pawnDirection == 'desc') {
-        res.push([[1, 0]]);
 
-        if (r == 1 && isEmptySquare(r + 2, c))
+        auxR = r + 1;
+        auxC = 0;
+        if (isEmptySquare(auxR, auxC))
+            res.push([[1, 0]]);
+
+        if (r == 1 && isEmptySquare(r + 2, c)) // first movment
             res[0].push([2, 0]);
 
         auxR = r + 1;
@@ -78,7 +92,10 @@ function movesPawn(r, c, pawnDirection) {
         }
 
     } else {
-        res.push([[-1, 0]]);
+        auxR = r - 1;
+        auxC = 0;
+        if (isEmptySquare(auxR, auxC))
+            res.push([[-1, 0]]);
 
         if (r == 6 && isEmptySquare(r - 2, c))
             res[0].push([-2, 0]);
@@ -119,7 +136,7 @@ function updateBoard(newBoard) {
 /*
     check color either is a white or black color
 */
-function isPieceWhite(r, c) {
+function isPieceWhite(r,c) {
     return document.getElementById("" + r + c).src.includes("b-") ? false : true;
 }
 /*
@@ -136,46 +153,95 @@ function isPieceInBoard(r, c) {
     return (r >= 0 && r <= 7) && (c >= 0 && c <= 7);
 }
 
+/*
+    Clear the destination if mouse leave board
+    and turn_phase == 1
+*/
+function onLeaveBoard(){
+    if (turn_phase == 1 )
+        clearSquareDestination();
+}
+
+/* 
+   Update the match message
+*/
+function setMatchMsg(msg) {
+    let color = is_turn_white ? "White" : "Black";
+
+    document.getElementById("msgPlayer").innerHTML = `${color}, ${msg}`; 
+}
+
+/*
+  Move a piece
+*/
+function movePiece(rowD, colD) {
+    let rowO = Number(square_orig[0]);
+    let colO = Number(square_orig[1]);
+
+    board[rowD][colD] = board[rowO][colO];
+    
+    board[rowO][colO] = null;
+    
+    updateBoard(board);
+    
+    clearSquareDestination();
+}
+
+
+/* 
+   Clear all squares marked as destination of a piece
+*/
+function clearSquareDestination(){
+    square_dests.map(square => document.getElementById( "d".concat(square )).classList.remove("b-square-destination"));
+    square_dests = [];
+}
+
 
 /* 
    mark on board all possibles destination 
 */
-function markDestination(target) {
+function chooseDest(target) {
     let row;
     let col;
     let rowD;
     let colD;
     let positions;
-    let pawnDirection = isTurnWhite ? wPawnDirection : bPawnDirection;
+    let pawnDirection = is_turn_white ? w_pawn_direction : b_pawn_direction;
 
-    row = Number(target.children[0].id[0]);
-    col = Number(target.children[0].id[1]);
+    row = Number(target.children[1].id[0]);
+    col = Number(target.children[1].id[1]);
+
+    if ( turn_phase == 2 )
+        return
+    
 
     // clear previous lists
-    square_destination.map(square => document.getElementById(square).classList.remove("b-board-destination"));
+    clearSquareDestination();
 
     if (isEmptySquare(row, col))
         return
-
+ 
     // check if a game has starte
     if (document.getElementsByClassName("b-placar")[0].style.display != "flex"){
         return
     }
 
     // check if selected piece is the same color oh the turn
-    if (isTurnWhite != isPieceWhite(row, col)) {
+    if (is_turn_white != isPieceWhite(row, col)) {
         return
     }
 
-    if (target.children[0].src.includes("pawn"))
+    square_orig = "" + row + col;
+
+    if (target.children[1].src.includes("pawn"))
         positions = movesPawn(row, col, pawnDirection)
-    else if (target.children[0].src.includes("knight"))
+    else if (target.children[1].src.includes("knight"))
         positions = moves_knight
-    else if (target.children[0].src.includes("bishop"))
+    else if (target.children[1].src.includes("bishop"))
         positions = moves_bishop;
-    else if (target.children[0].src.includes("hook"))
+    else if (target.children[1].src.includes("hook"))
         positions = moves_hook;
-    else if (target.children[0].src.includes("queen"))
+    else if (target.children[1].src.includes("queen"))
         positions = moves_queen;
     else
         positions = moves_king;
@@ -188,15 +254,15 @@ function markDestination(target) {
             if (isPieceInBoard(rowD, colD)) {
 
                 if (isEmptySquare(rowD, colD)) {
-                    document.getElementById("" + rowD + colD).classList.add("b-board-destination");
-                    square_destination.push("" + rowD + colD);
+                    document.getElementById("d" + rowD + colD).classList.add("b-square-destination");
+                    square_dests.push("" + rowD + colD);
                 } else {
 
                     if (isPieceWhite(row, col) == isPieceWhite(rowD, colD))
                         break
                     else {
-                        document.getElementById("" + rowD + colD).classList.add("b-board-destination");
-                        square_destination.push("" + rowD + colD);
+                        document.getElementById("d" + rowD + colD).classList.add("b-square-destination");
+                        square_dests.push("" + rowD + colD);
                         break
                     }
                 }
@@ -204,6 +270,46 @@ function markDestination(target) {
         }
     }
 }
+
+/*
+    this function will handle movements
+*/
+function handleMove(target) {
+    let row;
+    let col;
+    let auxId = target.children[1].id;
+
+    if (auxId[0] == "d") {
+        row = Number(target.children[1].id[1]);
+        col = Number(target.children[1].id[2]);
+    } else {
+        row = Number(target.children[1].id[0]);
+        col = Number(target.children[1].id[1]);        
+    }
+
+
+    if (turn_phase == 1) { // choosing piece phase
+
+        if (isEmptySquare(row, col) || isPieceWhite(row, col) != is_turn_white) 
+            return
+    
+        setMatchMsg("choose a destination ( ESC cancel )"); 
+
+        turn_phase = 2;            
+    } else {
+        
+        if ( square_dests.indexOf("" + row + col) < 0) 
+            return
+
+        movePiece(row, col);
+        is_turn_white = !is_turn_white;      
+        turn_phase = 1;
+        setMatchMsg("choose a piece"); 
+
+    }
+}
+
+
 /*
     this function will reset scenario for a new game
 */
@@ -216,15 +322,18 @@ function newGame() {
     document.getElementById("player1").focus();
 
     if (Math.random() < .5) {
-        updateBoard([w_piece, w_pawn, n_line, n_line, n_line, n_line, b_pawn, b_piece]);
-        wPawnDirection = 'asc';
-        bPawnDirection = 'desc';
+        updateBoard([[...w_piece], [...w_pawn], [...n_line], [...n_line], [...n_line], [...n_line], [...b_pawn], [...b_piece]]);
+        b_pawn_direction = 'asc';
+        w_pawn_direction = 'desc';
+
     } else {
-        updateBoard([b_piece, b_pawn, n_line, n_line, n_line, n_line, w_pawn, w_piece]);
-        bPawnDirection = 'asc';
-        wPawnDirection = 'desc';
+        updateBoard([[...b_piece], [...b_pawn], [...n_line], [...n_line], [...n_line], [...n_line], [...w_pawn], [...w_piece]]);
+        w_pawn_direction = 'asc';
+        b_pawn_direction = 'desc';
     }
-    isTurnWhite = true;
+    is_turn_white = true;
+
+    setMatchMsg("choose a piece to move"); 
 }
 
 /*
@@ -237,18 +346,33 @@ function startGame() {
     document.getElementsByClassName("b-placar")[0].style.display = "flex";
 }
 
-// draw the board
-updateBoard([
-    ["t", "h", "b", "k", "q", "b", "h", "t"],
-    ["p", "p", "p", "p", null, "p", null, "p"],
-    [null, null, null, null, "p", null, "p", null],
-    [null, null, null, null, null, "P", null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, "K", "P", "Q", null, "T", null],
-    ["T", null, "B", "H", "B", null, "H", "P"],
-    ["P", "P", "P", null, "P", null, "P", null]
-]);
+w_pawn_direction = 'asc';
+b_pawn_direction = 'desc';
+is_turn_white = true;
+turn_phase = 1;
 
-wPawnDirection = 'asc';
-bPawnDirection = 'desc';
-isTurnWhite = true;
+
+// // draw the board
+// updateBoard([
+//     ["t", "h", "b", "k", "q", "b", "h", "t"],
+//     ["p", "p", "p", "p", null, "p", null, "p"],
+//     [null, null, null, null, "p", null, "p", null],
+//     [null, null, null, null, null, "P", null, null],
+//     [null, null, null, null, null, null, null, null],
+//     [null, null, "K", "P", "Q", null, "T", null],
+//     ["T", null, "B", "H", "B", null, "H", "P"],
+//     ["P", "P", "P", null, "P", null, "P", null]
+// ]);
+
+
+function repeatPieceChossing(key){ 
+    turn_phase = 1;
+    clearSquareDestination();
+    setMatchMsg("choose a piece to move"); 
+}
+
+
+newGame();
+document.getElementById("player1").value = "p1";
+document.getElementById("player2").value = "p2";
+startGame();
